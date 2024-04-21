acfb4d8b4ff566f5800b261fc42293cbe269931e
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-04-21 07:10:41 +0100

    Updated voting function and added mapping to track users vote

--- src/Governance.sol
+++ src/Governance.sol
@@ -2,9 +2,12 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "./PeerToken.sol";
 
 contract Governance {
+    // TODO: Add event and emit events for functions
     uint256 internal proposalId;
 
     enum ProposalType {
@@ -35,20 +38,29 @@ contract Governance {
     mapping(uint256 => Proposal) proposals;
 
     mapping(address => uint256) votingPower;
+    mapping(address => uint256) votingRewards;
+    mapping(address => mapping(uint256 => bool)) voted;
 
     function addVotingPower(uint256 _amount) public {
-        // transfer user token to contract
-        // IERC20.transferFrom(msg.sender, address(this), _amount);
-
+        IERC20 token = IERC20(address(this));
+        require(_amount > 0, "Amount must be greater than 0");
+        require(
+            token.allowance(msg.sender, address(this)) >= _amount,
+            "Not enough allowance"
+        );
+        token.transferFrom(msg.sender, address(this), _amount);
         votingPower[msg.sender] = votingPower[msg.sender] + _amount;
+
+        // emit event
     }
 
     function reduceVotingPower(uint256 _amount) public {
         // transfer user token to contract
-        // IERC20.transferFrom(msg.sender, address(this), _amount);
         require(votingPower[msg.sender] >= _amount, "Not Enough voting power");
+        IERC20 token = IERC20(address(this));
 
         votingPower[msg.sender] = votingPower[msg.sender] - _amount;
+        token.transfer(msg.sender, _amount);
     }
 
     function createProposal(
@@ -67,6 +79,8 @@ contract Governance {
         _newProposal.status = Status.PENDING;
         _newProposal.deadline = block.number + _deadline;
 
+        _newProposal.vote_count = new uint256[](_options.length);
+
         proposals[proposalId] = _newProposal;
 
         proposalId = proposalId + 1;
@@ -79,16 +93,22 @@ contract Governance {
     }
 
     function vote(uint256 _id, uint256 _option) public {
+        // TODO: check proposal is active
         require(votingPower[msg.sender] > 0, "Not enough voting power");
+        require(!voted[msg.sender][_id], "Already voted");
 
         Proposal storage _proposal = proposals[_id];
 
         require(_option < _proposal.options.length, "Option does not exist");
         uint256 _userVotePower = votingPower[msg.sender];
 
+        voted[msg.sender][_id] = true;
+
         _proposal.vote_count[_option] =
             _proposal.vote_count[_option] +
             _userVotePower;
+
+        // issue voting rewards
 
         // emit event
     }
