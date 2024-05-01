475d12c9190e28f6078967c8e8dddb928b1109d7
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-05-01 09:12:38 +0100

    updated deploy script to include sepolia addresses for test tokens and price feeds

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -25,20 +25,31 @@ contract DeployScript is Script {
     address[] _tokenAddresses;
     address[] _priceFeedAddresses;
 
-    address daiToken = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    // address daiToken = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     // address USDCAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address WETHAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    // // address WETHAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    //SEPOLIA TESTNET ADDRESSES
+    address daiToken = 0x3e622317f8C93f7328350cF0B56d9eD4C620C5d6;
+    address linkToken = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+    address USDCToken = 0xf08A50178dfcDe18524640EA6618a1f965821715;
 
     address daiPriceFeed = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
-    address WETHPriceFeed = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419; //ETH-USD
+    address linkPriceFeed = 0xc59E3633BAAC79493d908e63626716e204A45EdF;
+    address usdcPriceFeed = 0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E;
+    // address WETHPriceFeed = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419; //ETH-USD
 
     function setUp() public {
         _tokenAddresses.push(daiToken);
+        _tokenAddresses.push(linkToken);
+        _tokenAddresses.push(USDCToken);
         // tokens.push(USDCAddress);
-        _tokenAddresses.push(WETHAddress);
+        // _tokenAddresses.push(WETHAddress);
 
         _priceFeedAddresses.push(daiPriceFeed);
-        _priceFeedAddresses.push(WETHPriceFeed);
+        _priceFeedAddresses.push(linkPriceFeed);
+        _priceFeedAddresses.push(usdcPriceFeed);
+        // _priceFeedAddresses.push(WETHPriceFeed);
     }
 
     function run() public {
